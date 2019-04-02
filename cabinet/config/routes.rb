62469eb126a0c458cd21745325d12cd6cd633126
Dvontre Coleman <dvontrec@gmail.com>
2019-04-02 14:47:59 -0400

    Create docs controller

--- cabinet/config/routes.rb
+++ cabinet/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # set the root route to be welcoe controllers index action
   root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # create routes for actions adding to app
+  resources :docs
+
 end
