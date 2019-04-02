50eabd4680eb9588c0afbced437332f5d365dbfe
Dvontre Coleman <dvontrec@gmail.com>
2019-04-02 14:32:30 -0400

    Set root welcome page

--- cabinet/config/routes.rb
+++ cabinet/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  # set the root route to be welcoe controllers index action
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
