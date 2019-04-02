153925bd919eb06e9a865e8a4f6cc778b7d30af9
Dvontre Coleman <dvontrec@gmail.com>
2019-04-02 09:56:45 -0400

    Add print and numbers

---
+++ hello.rb
@@ -0,0 +1,18 @@
+# prints hello to the screen with a linebreak
+puts "hello"
+
+# prints hello without a linebreak
+print"hey "
+print"there. \n"
+
+
+# *******************************************************
+                  # NUMBERS
+# *******************************************************
+# basic numbers and number functions
+
+# addition
+puts 4+7
+
+# is the number even
+puts 32.even?
