83f3f553d2823be44374fd865cc6216220f8bfc8
Dvontre Coleman <dvontrec@gmail.com>
2019-04-02 10:00:33 -0400

    Begin strings stuff

--- hello.rb
+++ hello.rb
@@ -16,3 +16,15 @@ puts 4+7
 
 # is the number even
 puts 32.even?
+
+# prints out methods the number has
+# puts 37.methods
+
+# prints the digits individually from 1->10->100->1000 order
+puts 12673.digits
+
+
+# *******************************************************
+                  # STRINGS
+# *******************************************************
+# basic strings and string functions
