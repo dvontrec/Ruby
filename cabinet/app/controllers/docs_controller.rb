490b2e2ad05f67c9ecd7d776f5cd3abff7a6fe64
Dvontre Coleman <dvontrec@gmail.com>
2019-04-02 14:51:26 -0400

    Doc controller and routes

--- cabinet/app/controllers/docs_controller.rb
+++ cabinet/app/controllers/docs_controller.rb
@@ -1,2 +1,28 @@
 class DocsController < ApplicationController
+  # index
+  def index
+  end
+  # read
+  def show
+  end
+  # create (no view)
+  def new
+  end
+  # update (no view)
+  def edit
+  end
+  # destroy (no view)
+  def destroy
+  end
+
+  # private methods
+  private
+    # apply to entire controller DRY coding
+    def find_doc
+    end
+
+    def doc_params
+    end
+
+
 end
