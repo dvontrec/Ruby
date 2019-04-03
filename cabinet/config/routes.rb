3ae3f4a41ba066fb35f51f21f3b582171c3989e0
Dvontre Coleman <dvontrec@gmail.com>
2019-04-03 10:51:06 -0400

    Update root route for authenticated users

--- cabinet/config/routes.rb
+++ cabinet/config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-  # set the root route to be welcoe controllers index action
-  root 'welcome#index'
 
   # create routes for actions adding to app
   resources :docs
 
+  # if the user is authenticated
+  authenticated :user do 
+    root 'docs#index', as: 'authenticated_root'
+  end
+
+  # set the root route to be welcome controllers index action
+  root 'welcome#index'
 end
