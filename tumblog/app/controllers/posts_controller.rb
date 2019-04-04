bd8f0a704dc2645ab90f8df412b21abeaabbfae8
Dvontre Coleman <dvontrec@gmail.com>
2019-04-04 11:55:44 -0400

    Add post creation and display

--- tumblog/app/controllers/posts_controller.rb
+++ tumblog/app/controllers/posts_controller.rb
@@ -4,4 +4,20 @@ class PostsController < ApplicationController
   # new method (has view)
   def new
   end
+  # functionality for creating new post
+  def create
+    @post = Post.new(post_params)
+    @post.save
+
+    redirect_to @post
+  end
+  # show view
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
