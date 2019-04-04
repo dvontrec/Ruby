52b949b18bbbb60437b0826828d404191843f49d
Dvontre Coleman <dvontrec@gmail.com>
2019-04-04 16:44:00 -0400

    Update comment controller

--- tumblog/app/controllers/comments_controller.rb
+++ tumblog/app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+  def create
+    # finds the post
+    @post = Post.find(params[:post_id])
+    # creates a new comment on the post
+    @comment = @post.comments.create(params[:comment].permit(:name, :body))
+
+    # once the comment is created redirect to post
+    redirect_to post_path(@post)
+  end
 end
--- tumblog/app/models/comment.rb
+++ tumblog/app/models/comment.rb
@@ -1,14 +1,3 @@
 class Comment < ApplicationRecord
   belongs_to :post
-  def create
-    # finds the post
-    @post = Post.find(params[:post_id])
-    # creates a new comment on the post
-    @comment = @post.comments.create(params[:comment].permit(:name, :body))
-
-    # once the comment is created redirect to post
-    redirect_to post_path(@post)
-  end
-
-
 end
