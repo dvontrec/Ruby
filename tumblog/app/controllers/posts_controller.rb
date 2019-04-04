91adaaa15fd99d89a867ede4c12f2865771b7213
Dvontre Coleman <dvontrec@gmail.com>
2019-04-04 16:06:20 -0400

    Add edit and delete post

--- tumblog/app/controllers/posts_controller.rb
+++ tumblog/app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+
   def index
     # index will display all posts in descending order
     @posts = Post.all.order('created_at DESC')
@@ -23,8 +24,31 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params[:post].permit(:title, :body))
+      redirect_to @post
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :body)
+    end
+
+    def findPost
+
     end
 end
